2af21145add22c7591d9464dde80dbcf0e5f8aa8
Peter Becich <peterbecich@gmail.com>
2022-09-17 18:56:44 -0700

    build with Nix

---
+++ default.nix
@@ -0,0 +1,14 @@
+let
+  sources = import ./nix/sources.nix {};
+  haskellNix = import sources.haskellNix {};
+  pkgs = import
+    haskellNix.sources.nixpkgs-2205
+    haskellNix.nixpkgsArgs;
+in pkgs.haskell-nix.project {
+  projectFileName = "cabal.project";
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "halogen-chess";
+    src = ./.;
+  };
+  compiler-nix-name = "ghc8107";
+}
