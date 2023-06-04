d34b3b437bef16ca7538fd25ca3fb670ffaaaec2
Peter Becich <peterbecich@gmail.com>
2023-06-03 22:49:11 -0700

    update sources

--- flake.nix
+++ flake.nix
@@ -20,6 +20,10 @@
     let
       supportedSystems = [
         "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+
       ];
     in
       flake-utils.lib.eachSystem supportedSystems (system:
